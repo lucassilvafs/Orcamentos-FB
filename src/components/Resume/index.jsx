488e7ad90899be47d5d9dce2da05cf456bb1e19a
Lucas Silva <lucasfic2017@gmail.com>
2024-03-26 16:38:11 -0300

    tentativa do botao de compartilhar pdf com html2canvas e jspdf

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import * as C from "./styles";
 import { Modal } from 'antd';
 import html2canvas from 'html2canvas';
-
+import { jsPDF } from "jspdf";
 
 // const shareData = {
 //   title: 'Share', 
@@ -29,6 +29,31 @@ const Resume = ({ total, reloadPage, handleCheckout }) => {
       console.log('Shared successfully');
     });
   }
+
+  //Create PDf from HTML...
+function CreatePDFfromHTML(shareTarget) {
+  var HTML_Width = shareTarget.current.width();
+  var HTML_Height = shareTarget.current.height();
+  var top_left_margin = 15;
+  var PDF_Width = HTML_Width + (top_left_margin * 2);
+  var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+  var canvas_image_width = HTML_Width;
+  var canvas_image_height = HTML_Height;
+
+  var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+  html2canvas(shareTarget.current).then(function (canvas) {
+      var imgData = canvas.toDataURL("image/jpeg", 1.0);
+      var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+      pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+      for (var i = 1; i <= totalPDFPages; i++) { 
+          pdf.addPage(PDF_Width, PDF_Height);
+          pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+      }
+      pdf.save("Your_PDF_Name.pdf");
+      shareTarget.current.hide();
+  });
+}
 
   // const handleShare = () => {
   //   if (navigator.share && navigator.canShare(shareData)) {
@@ -71,7 +96,7 @@ const Resume = ({ total, reloadPage, handleCheckout }) => {
           <C.Total>R$ {total}</C.Total>
         </C.Footer>
         <C.ButtonDownload onClick={handleCheckout}>Gerar Orçamento</C.ButtonDownload>
-        <C.ButtonShare onClick={() => onShare(shareTarget)}>Compartilhar Arquivo</C.ButtonShare>
+        <C.ButtonShare onClick={() => CreatePDFfromHTML(shareTarget)}>Compartilhar Arquivo</C.ButtonShare>
         <C.ButtonErase onClick={handleOpenModalDelete}>Apagar Tudo</C.ButtonErase>
       </C.ResumeContainer>
     </C.Container>
